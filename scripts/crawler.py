300f9f0fdbede1ac812514550ba7e6652452d84f
li_eh_chao <470360046@qq.com>
2025-10-27 19:55:43 +0800

    提交一下

--- scripts/crawler.py
+++ scripts/crawler.py
@@ -1,1 +1,63 @@
-print('1123')
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+
+# 获取网页总数
+response_total = requests.get('https://www.gamer520.com/pcplay')
+soup_total = BeautifulSoup(response_total.text, 'html.parser')
+page_numbers = soup_total.find_all(class_='page-numbers')
+pageNum = 0
+if page_numbers:
+    li = page_numbers[0].find_all('li')
+    if li:
+        page = li[-2]
+        pageNum = int(page.find('a').text)
+        print(pageNum)
+conn = sqlite3.connect('ggbox.db')
+cursor = conn.cursor()
+# 创建表
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS games (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        pic TEXT NOT NULL,
+        href TEXT NOT NULL
+    )
+''')
+for i in range(pageNum):
+    print(i+1)
+    response = requests.get('https://www.gamer520.com/pcplay/page/'+str(i+1))
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # 查找元素
+    elements = soup.find_all(class_='col-lg-1-5 col-6 col-sm-6 col-md-4 col-lg-3')
+    # 遍历找到的元素
+    for element in elements:
+        name = ''
+        pic = ''
+        href = ''
+        entry_title = element.find_all(class_='entry-title')
+        lazyload = element.find_all(class_='lazyload')
+        if lazyload:
+            img = lazyload[0].get('data-src')
+            # 获取图标
+            pic = img
+            print(img)
+        if entry_title:  # 确保列表不为空
+            a = entry_title[0].find('a')
+            # 获取名称
+            name = a.text
+            print(a.text)
+            # 获取二级页面链
+            href = a.get('href')
+            print(a.get('href') if a else "未找到链接")
+        # 插入数据
+        cursor.execute("INSERT INTO games (name, pic, href) VALUES (?, ?, ?)", 
+                    (name, pic, href))
+# 提交事务
+conn.commit()
+# 关闭连接
+conn.close()
+    
+    
+    
--- src/components/GameLibraray.vue
+++ src/components/GameLibraray.vue
@@ -1,13 +1,48 @@
 <template>
     <div class="grid-fixed" :style="{ maxHeight: mainHeight, overflow: 'auto' }">
-        <div v-for="i in 123" class="content"></div>
+        <div v-for="item in users" :key="item.id" class="content">
+            <img :src="item.pic"></img>
+            <span class="title-overlay">{{item.name}}</span>
+        </div>
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted, computed } from 'vue'
+import initSqlJs from 'sql.js'
 let pyodide = null
+let db = null
+// 初始化数据库
+const initDatabase = async () => {
+    try {
+        const SQL = await initSqlJs({
+            locateFile: file => `https://sql.js.org/dist/${file}`
+        })
+        // 直接从 data 目录加载
+        const response = await fetch('./data/ggbox.db');
+        if (!response.ok) throw new Error('数据库文件未找到');
+
+        const arrayBuffer = await response.arrayBuffer();
+        const uint8Array = new Uint8Array(arrayBuffer);
+
+        db = new SQL.Database(uint8Array);
+        console.log('数据库加载成功！');
+
+        console.log('数据库初始化成功')
+    } catch (error) {
+        console.error('数据库初始化失败:', error)
+    }
+}
+// 分页状态
+const pagination = ref({
+    currentPage: 1,
+    pageSize: 20,
+    total: 0,
+    totalPages: 0
+})
 onMounted(async () => {
+    await initDatabase()
+    getGames()
     try {
         // 动态导入 Pyodide
         pyodide = await loadPyodide({
@@ -20,13 +55,40 @@ onMounted(async () => {
         console.log('Pyodide 初始化完成');
         await pyodide.runPythonAsync(`
 import micropip
-await micropip.install('numpy')
-import math
+await micropip.install('requests')
+print('12333')
 `)
     } catch (e) {
         console.log(`Pyodide 初始化失败: ${e.message}`)
     }
 })
+const users = ref([])
+const getGames = (page = 1) => {
+    if (!db) return
+    // 更新当前页码
+    pagination.value.currentPage = page
+
+    // 计算偏移量
+    const offset = (page - 1) * pagination.value.pageSize
+
+    // 获取总数
+    const countResult = db.exec('SELECT COUNT(*) as total FROM games')
+    const total = countResult[0]?.values[0][0] || 0
+    const result = db.exec(`SELECT * FROM games LIMIT ${pagination.value.pageSize} 
+    OFFSET ${offset}`)
+    if (result.length > 0) {
+        const columns = result[0].columns
+        const values = result[0].values
+
+        users.value = values.map(row => {
+            const games = {}
+            columns.forEach((col, index) => {
+                games[col] = row[index]
+            })
+            return games
+        })
+    }
+}
 const props = defineProps({
     height: {
         type: Number,
@@ -49,12 +111,33 @@ const mainHeight = computed(() => {
     gap: 15px;
 
     .content {
+        position: relative;
+        overflow: hidden;
         aspect-ratio: 1 / 1;
         width: 100%;
         /* 宽度占满网格单元格 */
         height: auto;
         /* 高度根据宽高比自动计算 */
         background-color: aqua;
+
+        img {
+            object-fit: cover;
+            /* 关键：填充整个容器 */
+            width: 100%;
+            height: 100%;
+        }
+
+        .title-overlay {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            background: linear-gradient(transparent, rgba(0, 0, 0, 0.7));
+            color: white;
+            padding: 8px 4px;
+            text-align: left;
+            font-size: 14px;
+            z-index: 2;
+        }
     }
 }
 </style>
